9c07ae38cb753aaf97ec5b8b36f0154a74e46c6d
jaedeokHan <hjaedeok@gmail.com>
2021-08-02 17:55:16 +0900

    [add] 6~7단은 for, while  반복문을 사용해서 해결

--- src/fun/gugudan/E03_six_seven/Main.java
+++ src/fun/gugudan/E03_six_seven/Main.java
@@ -1,2 +1,31 @@
-package fun.gugudan.E03_six_seven;public class Main {
+package fun.gugudan.E03_six_seven;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // 6단, 7단
+        // 반복문으로 해결
+        // for 문
+        for (int i = 6; i < 8; i++){
+            System.out.println(i + "단");
+            for (int j = 1; j < 10; j++){
+                System.out.print(i * j + " ");
+            }
+            System.out.println();
+        }
+
+        // while 문
+        int i = 6;
+        while (i < 8){
+            int j = 1;
+            System.out.println(i + "단");
+            while (j < 10){
+                System.out.print(i * j + " ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
 }
